5470d0ad12fa008d372a741cb3c4c393f1188c4d
LancerEnk <88569335+LancerEnk@users.noreply.github.com>
2023-03-06 14:08:58 +0800

    Add files via upload

---
+++ fuxian/test02/contracts/TestContract.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.7.0 <0.9.0;
+
+contract TestContract {
+    struct Vehicle {
+        uint256 status;
+    }
+
+    mapping(string => Vehicle) vehicles;
+
+    //返回车辆id
+    event Myevent(string res);
+
+    //当车辆状态改变后触发事件，返回车辆id
+    function setVehicleStatus(string memory uuid) public {
+        assert(vehicles[uuid].status == 0);
+        emit Myevent(uuid);
+        vehicles[uuid].status = 1;
+    }
+}
